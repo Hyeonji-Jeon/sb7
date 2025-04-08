a93ea6462d157d3b378a7977c0e16beb5682dcbd
Hyeonji Jeon <guswl1397@naver.com>
2025-04-08 11:15:16 +0900

    board test

--- src/test/java/org/zerock/sb7/board/BoardTests.java
+++ src/test/java/org/zerock/sb7/board/BoardTests.java
@@ -4,12 +4,15 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.zerock.sb7.board.repo.BoardRepo;
 import org.zerock.sb7.board.repo.FavoriteRepo;
 import org.zerock.sb7.board.repo.ReplyRepo;
+
+import java.util.Arrays;
 
 @SpringBootTest
 @Log4j2
@@ -26,6 +29,10 @@ public class BoardTests {
     public void testListGrouping() {
 
         Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Object[]> result = boardRepo.list1(pageable);
+
+        result.forEach(arr -> log.info(Arrays.toString(arr)));
 
     }
 
