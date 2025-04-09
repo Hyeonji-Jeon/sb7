1b5bf6c5916ad99ff10f5070479c1c11477ddcb6
Hyeonji Jeon <guswl1397@naver.com>
2025-04-09 10:22:09 +0900

    board test

--- src/main/java/org/zerock/sb7/board/repo/search/BoardSearchImpl.java
+++ src/main/java/org/zerock/sb7/board/repo/search/BoardSearchImpl.java
@@ -3,11 +3,13 @@ package org.zerock.sb7.board.repo.search;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.JPQLQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.zerock.sb7.board.domain.*;
+import org.zerock.sb7.board.dto.BoardListDTO;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -47,20 +49,28 @@ public class BoardSearchImpl implements BoardSearch {
         query.offset(offset);
         query.orderBy(new OrderSpecifier<>(Order.DESC, board.bno));
 
-        JPQLQuery<Tuple> listTupleQuery = query.select(
-                board.bno, board.title, board.writer, boardImage.fileName,
-                favorite.choice.eq(Choice.LIKE).countDistinct(),
-                reply.countDistinct());
+        JPQLQuery<BoardListDTO> dtoQuery = query.select(
+                Projections.bean(BoardListDTO.class,
+                        board.bno,
+                        board.title,
+                        board.writer,
+                        boardImage.fileName,
+                        favorite.choice.eq(Choice.LIKE).countDistinct().as("favoriteCount"),
+                        reply.countDistinct().as("replyCount")
+                )
+        );
+
+        log.info(dtoQuery.fetch());
 
 
-        log.info("----------------------------");
-
-        List<Tuple> results = listTupleQuery.fetch();
-
-        List<Integer> bnos =
-                results.stream().map(tuple -> tuple.get(0, Integer.class)).collect(Collectors.toUnmodifiableList());
-
-        log.info("Found {} boards", bnos);
+//        log.info("----------------------------");
+//
+//        List<Tuple> results = listTupleQuery.fetch();
+//
+//        List<Integer> bnos =
+//                results.stream().map(tuple -> tuple.get(0, Integer.class)).collect(Collectors.toUnmodifiableList());
+//
+//        log.info("Found {} boards", bnos);
 
 
     }
