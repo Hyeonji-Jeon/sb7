e6accb2170098ad8720117eae646ebea0291e0b6
Hyeonji Jeon <guswl1397@naver.com>
2025-04-09 14:19:23 +0900

    member test

--- src/main/java/org/zerock/sb7/sample/controller/SampleController.java
+++ src/main/java/org/zerock/sb7/sample/controller/SampleController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.sb7.member.dto.MemberDTO;
 import org.zerock.sb7.sample.controller.dto.SampleDTO;
 
 import java.util.List;
@@ -22,18 +23,17 @@ public class SampleController {
     @PreAuthorize("permitAll()")
     @GetMapping("ex1")
     public void ex1(){
-       log.info("ex1");
+        log.info("ex1");
     }
 
     //인증이 된(로그인된) 사용자들만 접근하게
     //isAuthorized( )
     @PreAuthorize("isAuthenticated()")
     @GetMapping("ex2")
-    public void ex2( @AuthenticationPrincipal UserDetails userDetails ) {
+    public void ex2( @AuthenticationPrincipal MemberDTO memberDTO ) {
         log.info("ex2");
         log.info("----------------------------");
-        log.info(userDetails.getUsername());
-        log.info(userDetails.getAuthorities());
+        log.info(memberDTO);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
@@ -51,8 +51,8 @@ public class SampleController {
         dto.setTitle("Sample Title by user02");
 
         model.addAttribute("dto", dto);
-
     }
+
 
     @PreAuthorize("#dto.author == authentication.name")
     @GetMapping("exDTO")
